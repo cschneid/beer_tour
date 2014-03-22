b8b2fc908d2a5d21e33023c1d09e964f41c95291
Chris Schneider <cschneider@comverge.com>
2014-03-21 20:23:18 -0600

    Initial Commit

---
+++ BeerTour/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  BeerTour
+//
+//  Created by Christopher Schneider on 3/21/14.
+//  Copyright (c) 2014 Razormoose LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
